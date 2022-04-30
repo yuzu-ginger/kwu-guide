b8504a6a992167284ff1796dc1923805912d32d7
Nina Nakajima <99044060+yuzu-ginger@users.noreply.github.com>
2022-05-01 00:59:11 +0900

    Update app.rb

--- menu1/app.rb
+++ menu1/app.rb
@@ -1,14 +1,6 @@
 require 'bundler/setup'
 require 'sinatra'
 require 'line/bot'
-require 'roo'
-require_relative 'bus'
-require_relative 'reply'
-require_relative 'certificate'
-require_relative 'question'
-require_relative 'question_char'
-require_relative 'room'
-require_relative 'richmenu'
 
 # test
 get '/' do
@@ -44,4 +36,106 @@ post '/callback' do
             end
         end
     end
+end
+
+def richmenu_url
+  choice1 = "京女ポータル"
+  choice2 = "図書館HP"
+  choice3 = "蔵書検索(OPAC)"
+  choice4 = "My Library"
+  {
+      "type": "template",
+      "altText": "リッチメニューURL",
+
+      "template": {
+          "type": "buttons",
+          "title": "ウェブページを開きます",
+          "text": "選んでください",
+
+          # ポストバックアクション
+          "actions": [
+              {
+                  "type": "uri",
+                  "label": choice1,
+                  "uri": "{※YOUR_URL※}"
+              },
+              {
+                  "type": "uri",
+                  "label": choice2,
+                  "uri": "{※YOUR_URL※}"
+              },
+              {
+                  "type": "uri",
+                  "label": choice3,
+                  "uri": "{※YOUR_URL※}"
+              },
+              {
+                  "type": "uri",
+                  "label": choice4,
+                  "uri": "{※YOUR_URL※}"
+              }
+          ]
+      }
+  }
+end
+
+def richmenu_qa
+  choice1 = "各種証明書"
+  choice2 = "Q&A"
+  {
+      "type": "template",
+      "altText": "リッチメニューURL",
+
+      "template": {
+          "type": "buttons",
+          "title": "choice",
+          "text": "どちらか選んでください",
+
+          # ポストバックアクション
+          "actions": [
+              {
+                  "type": "postback",
+                  "label": choice1,
+                  "displayText": choice1,
+                  "data": "#{choice1}"
+              },
+              {
+                  "type": "postback",
+                  "label": choice2,
+                  "displayText": choice2,
+                  "data": "#{choice2}"
+              }
+          ]
+      }
+  }
+end
+
+def richmenu_box
+    choice1 = "意見箱"
+    choice2 = "使い方"
+    {
+        "type": "template",
+        "altText": "意見箱使い方",
+  
+        "template": {
+            "type": "buttons",
+            "title": "choice",
+            "text": "どちらか選んでください",
+  
+            # ポストバックアクション
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": choice1,
+                    "displayText": choice1,
+                    "data": "#{choice1}"
+                },
+                {
+                    "type": "uri",
+                    "label": choice2,
+                    "uri": "{※YOUR_URL※}"
+                }
+            ]
+        }
+    }
 end
